22f45af05b071befe7d2bbb9bfa6bd6fd6b7ed22
skander-gh <94468839+skander-gh@users.noreply.github.com>
2021-12-08 15:13:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Formss from './forms.js';
+function App() {
+  return (
+    <div className="App">
+      <Formss></Formss>
+    </div>
+  );
+}
+
+export default App;
